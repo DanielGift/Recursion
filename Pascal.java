375f12de82327f6291416e5d09d75def851889eb
Daniel Gift <daniel.gift@sbs.org>
2024-03-06 11:16:04 -0500

    Create Pascal.java

---
+++ Pascal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class pascal
+{
+    /*THIS SHOULD BE CHANGED TO HIGHER NUMBERS FOR TESTING!!!*/
+    private static int numRows = 1; //sets the levels of recursion; this starts at 0 but you should test it at larger values
+    /*****************************/
+    public static void main(){
+        ArrayList<Integer> init = new ArrayList<Integer>();
+        init.add(1);
+        makerow(init, numRows);
+    }
+    
+    public static void makerow(ArrayList<Integer> arr, int n){
+        //To be implemented with recursion and a call to printrow
+    }
+    public static void printrow(ArrayList<Integer> arr){
+        int initblank = (numRows*2-1 - (arr.size()*2-1))/2;
+        for (int i = 0; i < initblank; i++){
+            System.out.print(" ");
+        }
+        for (int i:arr){
+            System.out.print(i + " " );
+        }
+        System.out.println();
+    }
+}
